ff961a23e97f87565c690733a9adc33bf031a2c9
chris-revell <cr395@cam.ac.uk>
2018-10-05 16:43:48 -0400

    Updates

--- TrainingDiary.py
+++ TrainingDiary.py
@@ -1,33 +1,55 @@
 import numpy as np
+import numpy.ma as ma
 import pandas as pd
 import matplotlib.pyplot as plt
 import activityio as aio
 from scipy.stats import binned_statistic
 import os.path, time
 
-data = aio.read("testfile2.fit")
+data = aio.read("testfile3.fit")
 
-fig,ax = plt.subplots(figsize=(10,4))
+fig,ax = plt.subplots(nrows=3,ncols=1,figsize=(10,7))
 
-print("Pick a data series to plot:")
-name = input("("+", ".join(list(data))+")\n")
-type(name)
+#print("Pick a data series to plot:")
+#name = input("("+", ".join(list(data))+")\n")
+#type(name)
 
-print(time.localtime(os.path.getmtime('testfile2.fit')))
-print(time.localtime(os.path.getmtime('testfile2.fit')))
+print(time.localtime(os.path.getmtime('testfile3.fit')))
+print(time.localtime(os.path.getmtime('testfile3.fit')))
 
+caddataarray = data["cad"].values
+for i,m in enumerate(caddataarray):
+    if m > 35:
+        caddataarray[i] = caddataarray[i-1]
+    else:
+        pass
+means,bin_edges,binnumber = binned_statistic(data.time.values/np.timedelta64(1, 'm'),caddataarray,bins=len(data.time.values)/5)
+ax[0].plot(bin_edges[:-1]+(bin_edges[1]-bin_edges[0])/2,means)
+ax[0].axhline(y=20,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=22,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=24,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=26,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=28,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=30,ls="--",color="Grey",alpha=0.3)
+ax[0].axhline(y=32,ls="--",color="Grey",alpha=0.3)
+ax[0].set_ylim([10,40])
+ax[0].set_ylabel("Stroke Rate")
 
+hrdataarray = data["hr"].values
+means,bin_edges,binnumber = binned_statistic(data.time.values/np.timedelta64(1, 'm'),hrdataarray,bins=len(data.time.values)/5)
+ax[1].plot(bin_edges[:-1]+(bin_edges[1]-bin_edges[0])/2,means)
+ax[1].axhspan(96, 115, facecolor='grey', alpha=0.3)
+ax[1].axhspan(115, 134, facecolor='blue', alpha=0.3)
+ax[1].axhspan(134, 154, facecolor='green', alpha=0.3)
+ax[1].axhspan(154, 173, facecolor='orange', alpha=0.3)
+ax[1].axhspan(173, 193, facecolor='red', alpha=0.3)
+ax[1].set_ylabel("Heart Rate")
 
-
-means,bin_edges,binnumber = binned_statistic(data.time.values/np.timedelta64(1, 'm'),data[name].values,bins=len(data.time.values)/5)
-ax.plot(bin_edges[:-1]+(bin_edges[1]-bin_edges[0])/2,means)
-if (name=="hr"):
-    ax.axhspan(96, 115, facecolor='grey', alpha=0.3)
-    ax.axhspan(115, 134, facecolor='blue', alpha=0.3)
-    ax.axhspan(134, 154, facecolor='green', alpha=0.3)
-    ax.axhspan(154, 173, facecolor='orange', alpha=0.3)
-    ax.axhspan(173, 193, facecolor='red', alpha=0.3)
-fig.savefig("{}test.png".format(name),dpi=500,bbox_inches='tight',padding_inches=0)
-ax.set_xlabel("Time")
-ax.set_ylabel(name)
+speeddataarray = data["speed"].values
+means,bin_edges,binnumber = binned_statistic(data.time.values/np.timedelta64(1, 'm'),speeddataarray,bins=len(data.time.values)/5)
+ax[2].plot(bin_edges[:-1]+(bin_edges[1]-bin_edges[0])/2,means)
+ax[2].set_xlabel("Time")
+ax[2].set_ylabel("Speed")
+ax[2].set_ylim([2,4.5])
+fig.savefig("test.png",dpi=500,bbox_inches='tight',padding_inches=0)
 plt.show()
